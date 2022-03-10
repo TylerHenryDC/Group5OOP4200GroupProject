d8c8db9605a550a888920474738207db7ae73460
Nicholas Shortt <nicholas.shortt@hotmail.com>
2022-03-10 18:32:44 -0500

    Defined the player class

--- Group5OOP4200GroupProject/Class/Player.cs
+++ Group5OOP4200GroupProject/Class/Player.cs
@@ -8,5 +8,59 @@ namespace Group5OOP4200GroupProject.Class
 {
     class Player
     {
+        // Class Variables
+        protected List<Card> hand;
+        protected int id;
+
+        //Constructors
+
+        /// <summary>
+        /// Initialize the player
+        /// </summary>
+        /// <param name="id">Player id</param>
+        Player(int id)
+        {
+            this.id = id;
+        }
+
+        // Methods
+
+        /// <summary>
+        /// Check if a given card exists in the players hand
+        /// </summary>
+        /// <param name="card">Card to check for</param>
+        /// <returns>True if found false if not</returns>
+        public bool checkHand(Card card)
+        {
+            return hand.Contains(card);
+        }
+
+        /// <summary>
+        /// Add a given new card to the hand list
+        /// </summary>
+        /// <param name="card">Card to add</param>
+        public void addCard(Card card)
+        {
+            hand.Add(card);
+        }
+
+        /// <summary>
+        /// Remove specified card form hand
+        /// </summary>
+        /// <param name="card">Card to remove</param>
+        public void removeCard(Card card)
+        {
+            hand.Remove(card);
+        }
+
+        // Properties
+
+        /// <summary>
+        /// Gets the player id number
+        /// </summary>
+        public int ID
+        {
+            get { return id; }
+        }
     }
 }
